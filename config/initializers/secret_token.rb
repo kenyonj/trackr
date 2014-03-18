33c06cbcf3020156279d53b9dff10bc4fd85ff0b
Justin Kenyon <kenyonj@gmail.com>
2014-03-17 20:41:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trackr::Application.config.secret_key_base = 'b9371da9d4452acd119016276dff01c61e5351fc1e98eefdef2ddf56a02cad0268d2a9f3b6ca74f12b5e4e730c089ec1d0405e550ab7842c82ef78dbd5596092'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trackr::Application.config.session_store :cookie_store, key: '_trackr_session'
